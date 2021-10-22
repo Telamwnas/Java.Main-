d945cc50c34e3b2658a371cf2f81cea4d5d905d6
Elenis Marios <71108397+iknowyourpassword@users.noreply.github.com>
2021-10-22 17:38:17 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+mport java.util.Scanner;
+public class Main{
+   public static void main(String[] args) {
+      //ΔΙΛΩΣΗ ΜΕΤΑΒΛΗΤΩΝ 
+      double num1,num2,num3;
+      double ans;
+      //ΑΡΙΘΜΙΤΙΚΟΣ ΧΑΡΑΚΤΗΡΙΣΜΟΣ 
+      char op;
+      Scanner reader = new Scanner(System.in);
+      //ΕΙΣΟΔΟΣ ΑΡΙΘΜΟΝ ΑΠΟ ΤΟΝ ΧΡΗΣΤΗ
+      System.out.print("ΕΙΣΑΓΕΤΕ ΤΡΕΙΣ ΑΡΙΘΜΟΥΣ : ");
+      num1 = reader.nextDouble();
+      num2 = reader.nextDouble();
+      num3 = reader.nextDouble();
+      //ΕΙΣΟΔΟΣ ΔΥΑΔΙΚΟΥ ΤΕΛΕΣΤΗ ΑΠΟ ΤΟΝ ΧΡΗΣΗ
+      System.out.print("\nΕΠΙΛΕΞΤΕ ΔΥΑΔΙΚΟ ΤΕΛΕΣΤΗ(+, -, *, /) : ");
+      op = reader.next().charAt(0);
+      switch(op) {
+         case '+': ans = num1 + num2 + num3;
+            break;
+         case '-': ans = num1 - num2 - num3;
+            break;
+         case '*': ans = num1 * num2 * num3;
+            break;
+         case '/': ans = num1 / num2 / num3;
+           
+        default: System.out.printf("ΣΦΑΛΜΑ! ΕΙΣΑΓΕΤΕ ΣΩΣΤΟ ΤΕΛΕΣΤΗ");
+         return;
+      }
+      //ΠΡΑΞΕΙΣ ΤΟΝ ΑΡΙΘΜΟΝ
+      System.out.print("\nΤΟ ΑΠΟΤΕΣΜΑΣ ΕΙΝΑΙ :\n");
+      System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
+   }
+}
